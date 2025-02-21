551ae58bd91b1e55af2d9588c87fc8d62f329996
Mayanja Jessy Elijah <mjessyelijah@gmail.com>
2025-02-21 10:51:56 +0300

    added models directory for all models

---
+++ backend/aitsapp/models/__init__.py
@@ -0,0 +1,5 @@
+from .users import *          # Import everything from user.py
+from .auth import *          
+from .issues import *        
+from .comments import *      
+from .attachment import *    
