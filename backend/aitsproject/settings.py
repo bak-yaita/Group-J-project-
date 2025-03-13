8f817eea2deee266f0a4cf3d5c0a53ea8140fe5e
Mayanja Jessy Elijah <mjessyelijah@gmail.com>
2025-03-13 18:44:04 +0300

    modified settings.py file

--- backend/aitsproject/settings.py
+++ backend/aitsproject/settings.py
@@ -117,6 +117,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "aitsapp.User"
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  
+    ],
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
