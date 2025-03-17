bc0399da7f967860918bec0b50437630e9db4cbf
kaladi-esther <estherkaladdi@gmail.com>
2025-03-17 09:05:34 +0300

    removed profile

--- frontend/Issue -tracker/src/components/Navbar.jsx
+++ frontend/Issue -tracker/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
     <div>
-      
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/issues">Issues</a></li>
+          <li><a href="/create">Create Issue</a></li>
+          <li><a href="/login">Login</a></li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
