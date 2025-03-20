69281d07d6d4eead810c6352594f5bd869abd371
Mathias Atugonza <atumathias20@gmail.com>
2025-03-21 00:58:37 +0300

    resolved the conflicts in the authserializers.py
    modified the aitsapp/auth/authserializers.py

--- backend/aitsapp/urls.py
+++ backend/aitsapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from aitsapp.viewsets import UserViewSet, IssueViewSet, NotificationViewSet
-from aitsapp.viewsets.authentication_viewset import AuthenticationViewSet
+from aitsapp.viewsets import UserViewSet, IssueViewSet, NotificationViewSet, AuthenticationViewSet
    
 
 # Create a router and register ViewSets
@@ -9,8 +8,8 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'issues', IssueViewSet, basename='issue')
 router.register(r'notifications', NotificationViewSet, basename='notification')
+router.register(r'auth', AuthenticationViewSet, basename='auth')
 
-# API Endpoints for Users, Issues, and Notifications
 urlpatterns = [
     path('api/', include(router.urls)),  # Include all ViewSet routes
 ]
