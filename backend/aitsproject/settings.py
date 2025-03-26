93414d7d0ef2a68c450d47d9ce8e8f7e9ffa7cec
Mathias Atugonza <atumathias20@gmail.com>
2025-03-26 20:24:57 +0300

    i have modified the settings.py file to configure profile pictures

--- backend/aitsproject/settings.py
+++ backend/aitsproject/settings.py
@@ -143,6 +143,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Media files (profile pictures, etc.)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Ensure the media directory exists
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
