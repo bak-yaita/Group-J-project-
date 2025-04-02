ae43dadc4ed55afe4decd37bf1bc0d3c5ea3e766
Melissa Violet <melissavioletvs@gmail.com>
2025-04-02 16:45:19 +0300

    Added role based viewsets

--- backend/aitsapp/permissions.py
+++ backend/aitsapp/permissions.py
@@ -2,22 +2,22 @@ from rest_framework.permissions import BasePermission
 
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
-        return request.useris_authenticated and request.user.role == 'student'
+        return request.user.is_authenticated and request.user.role == 'student'
     
 
 class IsLecturer(BasePermission):
     def has_permission(self, request, view):
-        return request.useris_authenticated and request.user.role == 'lecturer'
+        return request.user.is_authenticated and request.user.role == 'lecturer'
     
 
 class IsRegistrar(BasePermission):
     def has_permission(self, request, view):
-        return request.useris_authenticated and request.user.role == 'registrar'
+        return request.user.is_authenticated and request.user.role == 'registrar'
     
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.useris_authenticated and request.user.role == 'admin'
+        return request.user.is_authenticated and request.user.role == 'admin'
     
 
     
--- backend/aitsapp/viewsets/dashboard_viewset.py
+++ backend/aitsapp/viewsets/dashboard_viewset.py
@@ -1,40 +1,27 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets 
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-from aitsapp.models.Users import User 
+from rest_framework.response import Response
+from aitsapp.permissions import IsStudent, IsRegistrar, IsLecturer
 
-class StudentDashboardView(APIView):
-    permission_classes = [IsAuthenticated]
+class StudentViewset(viewsets.ViewSet):
+    permission_classes= [IsStudent]
 
-    def get(self, request):
-        if request.user.role == "student":
-            return Response({"message": "Welcome to the Student Dashboard"}, status=status.HTTP_200_OK)
-        return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
+    def list(self,request):
+        username = request.user.username
+        return Response({"message": f"welcome, {username}"})
+    
+class LecturerViewset(viewsets.ViewSet):
+    permission_classes =[IsLecturer]
 
+    def list(self,request):
+        username = request.user.username
+        return Response({"message": f"Welcome, {username}"})
+    
+class RegistrarViewset(viewsets.ViewSet):
+    permission_classes =[IsRegistrar]
 
-class LecturerDashboardView(APIView):
-    permission_classes = [IsAuthenticated]
+    def list(self,request):
+        username = request.user.username
+        return Response({"message": f"Welcome, {username}"})
+    
 
-    def get(self, request):
-        if request.user.role == "lecturer":
-            return Response({"message": "Welcome to the Lecturer Dashboard"}, status=status.HTTP_200_OK)
-        return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
-
-
-class RegistrarDashboardView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        if request.user.role == "registrar":
-            return Response({"message": "Welcome to the Registrar Dashboard"}, status=status.HTTP_200_OK)
-        return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
-
-
-class AdminDashboardView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        if request.user.is_superuser:
-            return Response({"message": "Welcome to the Admin Dashboard"}, status=status.HTTP_200_OK)
-        return Response({"error": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
