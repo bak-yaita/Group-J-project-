15a72aa784171433c2602211ce5489a94450c657
Melissa Violet <melissavioletvs@gmail.com>
2025-04-02 16:47:54 +0300

    also work with IsAuthenticated class

--- backend/aitsapp/viewsets/dashboard_viewset.py
+++ backend/aitsapp/viewsets/dashboard_viewset.py
@@ -4,21 +4,21 @@ from rest_framework.response import Response
 from aitsapp.permissions import IsStudent, IsRegistrar, IsLecturer
 
 class StudentViewset(viewsets.ViewSet):
-    permission_classes= [IsStudent]
+    permission_classes= [IsAuthenticated,IsStudent]
 
     def list(self,request):
         username = request.user.username
         return Response({"message": f"welcome, {username}"})
     
 class LecturerViewset(viewsets.ViewSet):
-    permission_classes =[IsLecturer]
+    permission_classes =[IsAuthenticated,IsLecturer]
 
     def list(self,request):
         username = request.user.username
         return Response({"message": f"Welcome, {username}"})
     
 class RegistrarViewset(viewsets.ViewSet):
-    permission_classes =[IsRegistrar]
+    permission_classes =[IsAuthenticated,IsRegistrar]
 
     def list(self,request):
         username = request.user.username
