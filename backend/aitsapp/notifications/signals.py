fba6166ef53df4f63ec4dd6619f8d514e7f610a9
Melissa Violet <melissavioletvs@gmail.com>
2025-04-16 18:25:58 +0300

    Added AuditLogSerializer

--- backend/aitsapp/notifications/signals.py
+++ backend/aitsapp/notifications/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .utils import send_notification_email
 from django.contrib.auth import get_user_model
-from .models import Issue, Notification
+from models import Issue, Notification
 
 User = get_user_model() 
 
