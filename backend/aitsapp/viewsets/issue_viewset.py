99c3252d02d1dd0c91cc1311016c54a4e1af93ff
Mayanja Jessy Elijah <mjessyelijah@gmail.com>
2025-04-17 13:43:52 +0300

    added a missing parameter for resolvimg a function

--- backend/aitsapp/viewsets/issue_viewset.py
+++ backend/aitsapp/viewsets/issue_viewset.py
@@ -80,7 +80,7 @@ class IssueViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post',])
     def assign(self, request, pk=None):
         issue = self.get_object()
 
@@ -113,7 +113,7 @@ class IssueViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post','get'])
     def resolve(self, request, pk=None):
         issue = self.get_object()
 
